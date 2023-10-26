c2509005af8181b44e9e51eb459f1b35c720baa6
Vipul Kumar <vipulkr0808@gmail.com>
2023-10-27 02:05:59 +0530

    Add client

---
+++ client/src/data.js
@@ -0,0 +1,79 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://bit.ly/3P4VAVK",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/DG69bQ4/2.png",
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/cXFnLLV/3.png",
+    title: "LOUNGEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "SHIRT STYLE!",
+    cat: "shirt",
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/5560329/pexels-photo-5560329.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "LOUNGEWEAR LOVE",
+    cat: "loungewear",
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    title: "LIGHT JACKETS",
+    cat: "jackets",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: "https://www.vintageindustries.nl/download_front/tuzroi1804/_overview_large_/3542_Devin_T_shirt_Black.png",
+  },
+  {
+    id: 2,
+    img: "https://www.vintageindustries.nl/download_front/wzbmgl1928/_overview_large_/3543_Darwin_shirt_jacket_Birch_check.png",
+  },
+  {
+    id: 3,
+    img: "https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+  },
+  {
+    id: 4,
+    img: "https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+  },
+  {
+    id: 5,
+    img: "https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+  },
+  {
+    id: 6,
+    img: "https://www.vintageindustries.nl/download_front/schkyb1297/_overview_large_/5012_Corduroy_cap_Beige.png",
+  },
+  {
+    id: 7,
+    img: "https://assets.ajio.com/medias/sys_master/root/20211224/FNc4/61c4c6f6f997ddf8f15f07d0/-473Wx593H-469034062-black-MODEL.jpg",
+  },
+  {
+    id: 8,
+    img: "https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+  },
+];
