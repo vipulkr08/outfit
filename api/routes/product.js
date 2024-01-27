e9f0f57f9cbd43ff178b3ed30a34ded28b2bf596
Vipul Kumar <vipulkr0808@gmail.com>
2024-01-27 16:46:04 +0530

    Change product limit

--- api/routes/product.js
+++ api/routes/product.js
@@ -57,7 +57,7 @@ router.get("/", async (req, res) => {
   try {
     let products;
     if (qNew) {
-      products = await Product.find().sort({ createdAt: -1 }).limit(1);
+      products = await Product.find().sort({ createdAt: -1 }).limit(5);
     } else if (qCategory) {
       products = await Product.find({
         categories: {
